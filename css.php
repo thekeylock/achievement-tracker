abb682e97421e6e853fe885049f7e57b21708d87
thekeylock <reim.adema@icloud.com>
2025-02-07 19:29:04 +0100

    changed title, added searchbar, and changed leftbar

--- css.php
+++ css.php
@@ -1,50 +1,73 @@
-html{
-    height: 100%;
-    width: 100%;
-}
-body{
-    margin: 0;
-    padding: 0;
-    background-color: purple;
-}
+<style>
+    html{
+        height: 100%;
+        width: 100%;
+    }
+    body{
+        margin: 0;
+        padding: 0;
+        background-color: purple;
+    }
 
-.title{
-    margin: auto;
-    width: max-content;
-}
+    .title{
+        background-color: beige;
+        padding: 20px 0;
+        width: 100%;
+        height: 37px;
+    }
 
-.left-bar{
-    float: left;
-    width: max-content;
-    overflow: none;
-}
+    .title h1{
+        text-align: center;
+        margin: 0;
+        padding: 0;
+    }
 
-.search-bar{
-    background-color: brown;
-    size: 5;
-    margin: 0;
-    padding: 0;
-    width: 100%;
-    width: max-content;
-}
+    .left-bar{
+        float: left;
+        overflow: scroll;
+        height: calc(100vh);
+    }
 
-.game{
-    background-color: blue;
-    padding: 10px;
-    font-size: 150%;
-    height: min-content;
-    width: 100%;
-}
-.game:hover{
-    background-color: darkblue;
-}
-.achievement{
-    background-color: blue;
-    height: min-content;
-    width: 100%;
-    padding: 10px;
-}
+    .search-bar{
+        background-color: brown;
+        margin: 0;
+        width: max-content;
+        height: 48px;
+    }
 
-.achievement:hover{
-    background-color: darkblue;
-}
+    .search-bar input{
+        vertical-align: top;
+        padding: 0;
+        border: 0;
+        height: 100%;
+        width: calc(100% - 48px);
+        font-size: 150%;
+    }
+
+    .search-bar button{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        height: 48px;
+        width: 48px;
+    }
+
+    .game{
+        background-color: darkslateblue;
+        padding: 10px 5%;
+        font-size: 150%;
+        height: min-content;
+        width: 90%;
+    }
+    .achievement:hover{
+        background-color: darkblue;
+        cursor: pointer;
+    }
+    .achievement{
+        text-align: ;
+        background-color: blue;
+        height: min-content;
+        width: 85%;
+        padding: 10px 5% 10px 10%;
+    }
+</style>
--- main.php
+++ main.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <title>Achievement Tracker</title>
 </head>
+<?php
+include("css.php");
+?>
 <body>
-    <h1 class="title">Achievement Tracker</h1>
+    <div class="title"><h1>Achievement Tracker</h1></div>
     <div class="left-bar">
         <form action="">
         <div class="search-bar"><input type="text" placeholder="Search" size="30"><button class="material-icons" type="submit">search</button></div>
@@ -20,6 +22,28 @@
         <div class="game">Marvel Rivals</div>
         <div class="achievement">play as groot</div>
         <div class="achievement">play as jeff</div>
+        <div class="game">Hollow Knight</div>
+        <div class="achievement">get money</div>
+        <div class="achievement">get a gun</div>
+        <div class="achievement">get a car</div>
+        <div class="game">Marvel Rivals</div>
+        <div class="achievement">play as groot</div>
+        <div class="achievement">play as jeff</div>
+        <div class="game">Hollow Knight</div>
+        <div class="achievement">get money</div>
+        <div class="achievement">get a gun</div>
+        <div class="achievement">get a car</div>
+        <div class="game">Marvel Rivals</div>
+        <div class="achievement">play as groot</div>
+        <div class="achievement">play as jeff</div>
+        <div class="game">Hollow Knight</div>
+        <div class="achievement">get money</div>
+        <div class="achievement">get a gun</div>
+        <div class="achievement">get a car</div>
+        <div class="game">Marvel Rivals</div>
+        <div class="achievement">play as groot</div>
+        <div class="achievement">play as jeff</div>
+
     </div>
 </body>
 </html>
