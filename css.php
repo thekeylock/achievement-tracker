c65effb93e1c743fc81277f16cca03acda7bda3e
thekeylock <reim.adema@icloud.com>
2025-02-07 17:24:28 +0100

    added left bar

--- css.php
+++ css.php
@@ -1,1 +1,50 @@
+html{
+    height: 100%;
+    width: 100%;
+}
+body{
+    margin: 0;
+    padding: 0;
+    background-color: purple;
+}
 
+.title{
+    margin: auto;
+    width: max-content;
+}
+
+.left-bar{
+    float: left;
+    width: max-content;
+    overflow: none;
+}
+
+.search-bar{
+    background-color: brown;
+    size: 5;
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    width: max-content;
+}
+
+.game{
+    background-color: blue;
+    padding: 10px;
+    font-size: 150%;
+    height: min-content;
+    width: 100%;
+}
+.game:hover{
+    background-color: darkblue;
+}
+.achievement{
+    background-color: blue;
+    height: min-content;
+    width: 100%;
+    padding: 10px;
+}
+
+.achievement:hover{
+    background-color: darkblue;
+}
