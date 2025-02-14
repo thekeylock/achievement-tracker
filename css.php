91a121f66daedf0db9d33bb7e31b6030044e3c01
thekeylock <reim.adema@icloud.com>
2025-02-14 17:59:18 +0100

    ben bezig geweest met outest bars en detailedachievement en addachievement

--- css.php
+++ css.php
@@ -2,6 +2,12 @@
     html{
         height: 100%;
         width: 100%;
+        min-width: 350px;
+    }
+
+    html::-webkit-scrollbar{
+        display: none;
+        width: 0;
     }
 
     body{
@@ -15,6 +21,7 @@
         padding: 20px 0;
         width: 100%;
         height: 37px;
+        white-space: nowrap;
     }
 
     .title h1{
@@ -23,12 +30,47 @@
         padding: 0;
     }
 
-    .left-bar{
+    .outest-left-bar{
+        background-color: chocolate;
         float: left;
+        width: 77px;
+        height: calc(100vh - 77px);
+    }
+
+    .outest-right-bar{
+        background-color: brown;
+        float: right;
+        width: 77px;
+        height: 100%;
+        height: calc(100vh - 77px);
+    }
+
+    .outer-left-bar{
+        height: calc(100vh - 77px);
+        float: left;
+        width: 400px;
+    }
+
+    .left-bar{
         overflow-y: scroll;
         overflow-x: none;
-        height: calc(100vh - 77px);
-        width: 500px;
+        height: 100%;
+        min-width: min-content;
+        max-width: 350px;
+    }
+
+    .left-left-bar{
+        background-color: orange;
+        width: 25px;
+        height: 100%;
+        float: left;
+    }
+
+    .right-left-bar{
+        background-color: orangered;
+        width: 25px;
+        height: 100%;
+        float: right;
     }
 
     .left-bar::-webkit-scrollbar{
@@ -68,7 +110,8 @@
         background-color: darkslateblue;
         padding: 10px 5%;
         font-size: 150%;
-        height: min-content;
+        white-space: nowrap;
+        height: max-content;
         width: 90%;
     }
 
@@ -79,16 +122,63 @@
 
     .achievement{
         background-color: blue;
-        height: min-content;
+        height: max-content;
         width: 85%;
         padding: 10px 5% 10px 10%;
+        white-space: nowrap;
     }
 
     .detailedachievement{
         background-color: green;
         float: right;
-        height: 100px;
-        min-width: 500px;
-        width: calc(100vw - 500px);
+        height: calc((100vh - 77px) / 2);
+        min-width: 400px;
+        width: calc(100vw - 400px - 77px * 2);
+    }
+
+    .addachievement{
+        background-color: red;
+        float: right;
+        height: calc((100vh - 77px) / 2);
+        min-width: 400px;
+        width: calc(100vw - 400px - 77px * 2);
+    }
+
+    @media screen and (max-width: 1000px) {
+        .outest-left-bar, .outest-right-bar{
+            width: calc(100vw - 923px);
+        }
+
+        .detailedachievement, .addachievement{
+            width: calc((100vw - 400px) + clamp(0, (100vw - 923px)) * 2);
+            /*hier was ik */
+        }
+    }
+
+
+    @media screen and (max-width: 800px) {
+
+        .outer-left-bar{
+            float: none;
+            height: calc(50vh - 77px);
+            width: 100%;
+            margin: auto;
+        }
+
+        .left-left-bar{
+            width: calc((100% - 350px)/2);
+        }
+
+        .right-left-bar{
+            width: calc((100% - 350px)/2);
+        }
+
+        .detailedachievement{
+            width: 100%;
+        }
+
+        .addachievement{
+            width: 100%;
+        }
     }
 </style>
--- main.php
+++ main.php
@@ -8,42 +8,53 @@
 </head>
 <?php
 include("css.php");
+
+function testytest(){
+    echo"Hello World";
+}
 ?>
 <body>
     <div class="title"><h1>Achievement Tracker</h1></div>
-    <div class="left-bar">
-        <form action="">
-        <div class="search-bar"><input type="text" placeholder="Search" size="30"><button class="material-icons" type="submit">search</button></div>
-        </form>
-        <div class="game">Hollow Knight</div>
-        <div class="achievement">get money</div>
-        <div class="achievement">get a gun</div>
-        <div class="achievement">get a car</div>
-        <div class="game">Marvel Rivals</div>
-        <div class="achievement">play as groot</div>
-        <div class="achievement">play as jeff</div>
-        <div class="game">Hollow Knight</div>
-        <div class="achievement">get money</div>
-        <div class="achievement">get a gun</div>
-        <div class="achievement">get a car</div>
-        <div class="game">Marvel Rivals</div>
-        <div class="achievement">play as groot</div>
-        <div class="achievement">play as jeff</div>
-        <div class="game">Hollow Knight</div>
-        <div class="achievement">get money</div>
-        <div class="achievement">get a gun</div>
-        <div class="achievement">get a car</div>
-        <div class="game">Marvel Rivals</div>
-        <div class="achievement">play as groot</div>
-        <div class="achievement">play as jeff</div>
-        <div class="game">Hollow Knight</div>
-        <div class="achievement">get money</div>
-        <div class="achievement">get a gun</div>
-        <div class="achievement">get a car</div>
-        <div class="game">Marvel Rivals</div>
-        <div class="achievement">play as groot</div>
-        <div class="achievement">play as jeff</div>
+    <div class="outest-left-bar"></div>
+    <div class="outest-right-bar"></div>
+    <div class="outer-left-bar">
+        <div class="left-left-bar"></div>
+        <div class="right-left-bar"></div>
+        <div class="left-bar">
+            <form action="">
+                <div class="search-bar"><input type="text" placeholder="Search" size="30"><button class="material-icons" type="submit">search</button></div>
+            </form>
+            <div class="game">Hollow Knight</div>
+            <div class="achievement" onclick="testytest()">get money</div>
+            <div class="achievement">get a gun</div>
+            <div class="achievement">get a car</div>
+            <div class="game">Marvel Rivals</div>
+            <div class="achievement">play as groot</div>
+            <div class="achievement">play as jeff</div>
+            <div class="game">Hollow Knight</div>
+            <div class="achievement">get money</div>
+            <div class="achievement">get a gun</div>
+            <div class="achievement">get a car</div>
+            <div class="game">Marvel Rivals</div>
+            <div class="achievement">play as groot</div>
+            <div class="achievement">play as jeff</div>
+            <div class="game">Hollow Knight</div>
+            <div class="achievement">get money</div>
+            <div class="achievement">get a gun</div>
+            <div class="achievement">get a car</div>
+            <div class="game">Marvel Rivals</div>
+            <div class="achievement">play as groot</div>
+            <div class="achievement">play as jeff</div>
+            <div class="game">Hollow Knight</div>
+            <div class="achievement">get money</div>
+            <div class="achievement">get a gun</div>
+            <div class="achievement">get a car</div>
+            <div class="game">Marvel Rivals</div>
+            <div class="achievement">play as groot</div>
+            <div class="achievement">play as jeff</div>
+        </div>
     </div>
     <div class="detailedachievement"></div>
+    <div class="addachievement"></div>
 </body>
 </html>
